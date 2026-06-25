1678fa12e3fab06af63e526f8ebe4d5a591e586e
CauaSCP <cauascp37@gmail.com>
2026-06-25 20:01:40 -0300

    flake not hard coded name

--- flake.nix
+++ flake.nix
@@ -5,62 +5,50 @@
     nixgl.url = "github:nix-community/nixGL";
     unstable-pkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  /*
-      path = builtins.toString ./.;
-      parts = builtins.filter (x: x != "") (builtins.split "/" path);
-      secondDir = builtins.elemAt parts 1;
-
-      # Define your variable here
-      username = secondDir;
-  */
-
   outputs = { self, nixgl, nixpkgs, unstable-pkgs, home-manager, ... }@inputs: 
     let
-      path = builtins.toString ./.;
-      parts = builtins.filter (x: x != "") (builtins.split "/" path);
-      secondDir = builtins.elemAt parts 1;
-
-      username = secondDir;
-      # # username = "caua";
+      username = builtins.getEnv "USER";
     in {
-      homeConfigurations = {
-        "${username}" = home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-          config.allowUnfree = true;
-          };
-
-          extraSpecialArgs = {
-              inherit inputs;
-            unstable-pkgs-unfree = import unstable-pkgs {
-                system = "x86_64-linux";
+      homeConfigurations = if username == "" then 
+        throw "Could not determine username from environment variable $USER. Please set it."
+        else {
+          "${username}" = home-manager.lib.homeManagerConfiguration {
+            pkgs = import nixpkgs {
+              system = "x86_64-linux";
               config.allowUnfree = true;
             };
 
-            unstable-pkgs = import unstable-pkgs {
-              system = "x86_64-linux";
-              config.allowUnfree = false;
-            };
+            extraSpecialArgs = {
+              inherit inputs;
 
-            nixGlOutput = import nixgl {
-                pkgs = import nixpkgs {
-                  system = "x86_64-linux";
+              unstable-pkgs-unfree = import unstable-pkgs {
+                system = "x86_64-linux";
                 config.allowUnfree = true;
               };
-            };
-          };
 
-          modules = [
-            ./home.nix
-          ];
+              unstable-pkgs = import unstable-pkgs {
+                system = "x86_64-linux";
+                config.allowUnfree = false;
+              };
+
+              nixGlOutput = import nixgl {
+                pkgs = import nixpkgs {
+                  system = "x86_64-linux";
+                  config.allowUnfree = true;
+                };
+              };
+            };
+
+            modules = [
+              ./home.nix
+            ];
+          };
         };
-      };
     };
 }
