ce796ce3ad1887fb077822f33cbcc0dd6421680a
CauaSCP <cauascp37@gmail.com>
2026-06-25 20:17:13 -0300

    flake fix1

--- flake.nix
+++ flake.nix
@@ -1,4 +1,17 @@
-outputs = { self, nixgl, nixpkgs, unstable-pkgs, home-manager, ... }@inputs: 
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixgl.url = "github:nix-community/nixGL";
+    unstable-pkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixgl, nixpkgs, unstable-pkgs, home-manager, ... }@inputs: 
   let
     # This runs in your shell, so it correctly gets "caua"
     username = builtins.getEnv "USER";
@@ -29,3 +42,4 @@ outputs = { self, nixgl, nixpkgs, unstable-pkgs, home-manager, ... }@inputs:
       modules = [ ./home.nix ];
     };
   };
+}
